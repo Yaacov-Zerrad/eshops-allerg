3b6750163814248931499736bc238955544337de
Yaacov-Zerrad <101695734+Yaacov-Zerrad@users.noreply.github.com>
2022-08-17 23:25:04 +0300

    add product

--- core/core/settings.py
+++ core/core/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'corsheaders',
     'djoser',
+    
+    'product',
 ]
 
 CORS_ALLOWED_ORIGINS = [
@@ -127,9 +129,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
